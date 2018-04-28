ce1e8d3f7129935df153ebd4c11a5f05b18c57b4
Chris Andersson <candersson@klarsolutions.com>
2018-04-28 21:47:42 +0000

    added edit and delete

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -24,9 +24,29 @@ class RecipesController < ApplicationController
      end
   end
   
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+      if @recipe.update(recipe_params)
+        flash[:success] = "Recipe was updated scuccesfully"
+        redirect_to recipe_path(@recipe)
+      else
+        render 'edit'
+      end
+  end
+  def destroy
+    Recipe.find(params[:id]).destroy
+    flash[:success] = "Recipe deleted succesfully"
+    redirect_to recipes_path
+  end
+  
   private
   
   def recipe_params
     params.require(:recipe).permit(:name, :description)
   end
+
 end
---
+++ test/integration/recipes_delete_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class RecipesDeleteTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = Chef.create!(chefname: "Chris", email: "candersson@klarsolutions.com")
+    @recipe = Recipe.create(name: "Vegetbale sautee", description: "great veggie sautee", chef: @user)
+  end
+
+
+  test "succesfully delete a recipe" do
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
+    assert_select 'a[href=?]', recipe_path(@recipe), text: "Delete this recipe"
+    assert_difference 'Recipe.count', -1 do
+      delete recipe_path(@recipe)
+    end
+    assert_redirected_to recipes_path
+    assert_not flash.empty?
+  end
+
+end
