be64cf644e0fff79e8fdc06bcf6a276961677322
Chris Andersson <candersson@klarsolutions.com>
2018-05-10 23:08:22 +0000

    added chef chat

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,9 @@ Rails.application.routes.draw do
  resources :ingredients, except: [:destroy]
  
  mount ActionCable.server => '/cable'
+ 
+ get '/chat', to: 'chatrooms#show'
+ 
+ resources :messages, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
